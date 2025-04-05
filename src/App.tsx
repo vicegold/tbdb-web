ecfc67d0003534487d5e8ecc28de6a1ca731abee
Laurids Düllmann <laurids@vicegold.com>
2025-04-05 17:29:39 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import tbdbLogo from '/tbdb.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>welcome to tbdb.net</h1>
+      <img src={tbdbLogo} alt="tbdb.net" />
+    </>
+  )
+}
+
+export default App
